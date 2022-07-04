1c4f66f73f5ba661a255f03cb68c1f0dbd85c5b6
Warren Pope <warrenpope2010@gmail.com>
2022-07-04 08:05:00 +0000

    Created user management system

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,17 +8,16 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path(route='', view = views.index.as_view(), name='index'),
     # path for about view
     path('about/', views.about, name='about'),
     # path for contact us view
     path('contact/', views.contact, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
